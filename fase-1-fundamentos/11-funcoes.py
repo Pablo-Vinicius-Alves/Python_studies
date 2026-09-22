97df4f4c87484fcc0c4038332835d0993290fd76
Pablo Vinícius Alves Pisolato <pblalves29@gmail.com>
2026-09-21 22:12:24 -0300

    Learning functions

--- fase-1-fundamentos/11-funcoes.py
+++ fase-1-fundamentos/11-funcoes.py
@@ -24,6 +24,21 @@ pessoa(nome="Pablo", idade=16, curso="DS")
 
 print("Calculadora")
 
+def soma(a, b):
+    result = a + b
+    return result
+    
+def subtracao(a, b):
+    result = a - b
+    return result
+
+def divisao(a, b):
+    result = a / b
+    return result
+    
+def multiplicacao(a, b):
+    result = a * b
+    return result
 
 while True:
     escolha = int(input("Qual operação vc vai fazer?\n1- Adição \n2- Subtração \n3- Divisão \n4- Multiplicação \n5- sair \n"))
@@ -36,5 +51,16 @@ while True:
     
     num1 = int(input("Digite o primeiro número: "))
     num2 = int(input("Digite o segundo número: "))
+    
+    if escolha == 1:
+        print(soma(num1, num2))
+    elif escolha == 2:
+        print(subtracao(num1, num2))
+    elif escolha == 3 and num2 == 0:
+        print("Erro! Divisão por 0")
+    elif escolha == 3:
+        print(divisao(num1, num2))
+    elif escolha == 4:
+        print(multiplicacao(num1, num2))
     
     
